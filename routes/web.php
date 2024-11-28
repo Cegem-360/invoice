f6d2e2b9d5e467a47df899e44aa7cdcde8ac5402
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2024-11-28 13:06:52 +0100

    Új CSV export funkció hozzáadása a termékekhez, a league/csv csomag integrálásával a termékadatok letöltéséhez.

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/admin');
 });
+Route::get('/products', function () {
+    //csv data with all products
+    //create csv file from products
+    $products = \App\Models\Product::all();
+    $csv = \League\Csv\Writer::createFromFileObject(new SplTempFileObject);
+    $csv->insertOne(['nev', 'sku', 'ean', 'price']);
+    foreach ($products as $product) {
+        $csv->insertOne([$product->nev, $product->sku, $product->ean, $product->price]);
+    }
+    $csv->download('products.csv');
+
+    // return response()->download(storage_path('app/products.csv'));
+});
 require __DIR__.'/auth.php';
